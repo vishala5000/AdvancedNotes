812580fba9888a8555ef66b263ea38bf0587b82d
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-19 10:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+import tkinter as tk
 
+from database import Database
+from ui import NotesApplication
+
+
+def main():
+    root = tk.Tk()
+
+    database = Database()
+
+    application = NotesApplication(
+        root=root,
+        database=database
+    )
+
+    root.protocol("WM_DELETE_WINDOW", application.close)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
